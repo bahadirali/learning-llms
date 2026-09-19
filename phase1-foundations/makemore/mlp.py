b0773b9a39e4ce91f2e882d8f8f0d682858be41a
bahadirali <bahadirali25@gmail.com>
2026-09-18 22:04:56 -0500

    phase1: add sampling loop to generate names from trained MLP

--- phase1-foundations/makemore/mlp.py
+++ phase1-foundations/makemore/mlp.py
@@ -63,6 +63,23 @@ for k in range(100000):
         p.data += -lr * p.grad
     print(loss.item())
 
+#sampling
+g = torch.Generator().manual_seed(2147483647)
+for _ in range(20):
+    out = []
+    context = [0] * block_size
+    while True:
+        emb = C[torch.tensor([context])]
+        h = torch.tanh(emb.view(1, -1) @ W1 + b1)
+        logits = h @ W2 + b2
+        probs = F.softmax(logits, dim=1)
+        ix = torch.multinomial(probs, num_samples=1, generator=g).item()
+        context = context[1:] + [ix]
+        out.append(ix)
+        if ix == 0:
+            break
+    print(''.join(itos[i] for i in out))
+
 
 emb = C[Xtr]
 h = torch.tanh(emb.view(-1, block_size * emb_dim) @ W1 + b1)
